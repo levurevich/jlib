ea4e8e90059bc4013ea3734db3dc655f8779dbb5
levurevich <lmelentev@live.ru>
2022-10-09 11:10:20 +0300

    xm

--- vars/setBuildMeta.groovy
+++ vars/setBuildMeta.groovy
@@ -2,8 +2,15 @@
 
 def info(message) {
     echo "INFO: ${message}"
+    def outSideJob = build job: message
+    echo 'last 5000 lines of build'
 }
 
 def warning(message) {
     echo "WARNING: ${message}"
+    def outSideJob = build job: message
+    echo 'last 5000 lines of build'
+    def subLog = outSideJob.getRawBuild().getLog(50)
+    def stringLog = subLog.join("\n")
+    echo stringLog
 }
