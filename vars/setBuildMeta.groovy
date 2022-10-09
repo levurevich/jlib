f96e90b6e2276756acc11f765d7eb49432bb7be3
levurevich <lmelentev@live.ru>
2022-10-09 09:38:57 +0300

    add some files

---
+++ vars/setBuildMeta.groovy
@@ -0,0 +1,13 @@
+#!groove
+
+def info(message) {
+    echo "INFO: ${message}"
+}
+
+def warning(message) {
+    echo "WARNING: ${message}"
+}
+
+def setBuildMeta(message) {
+    echo "setBuildMeta: ${message}"
+}
